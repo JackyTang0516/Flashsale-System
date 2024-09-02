62d28bbf344b2552bb4d3e5bee8050c6ac1a2a1d
jacky <jackytang0516@gmail.com>
2024-09-01 23:32:32 -0700

    update sql

--- seckill_activity.sql
+++ seckill_activity.sql
@@ -1,19 +1,4 @@
-/*
- Navicat Premium Data Transfer
-
- Source Server         : localhost_MySQL
- Source Server Type    : MySQL
- Source Server Version : 80021
- Source Host           : localhost:3306
- Source Schema         : seckill_jiuzhang
-
- Target Server Type    : MySQL
- Target Server Version : 80021
- File Encoding         : 65001
-
- Date: 15/11/2020 15:45:10
-*/
-
+-- Description: Seckill Activity Table
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
